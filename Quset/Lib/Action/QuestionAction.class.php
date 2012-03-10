4286beb182a944694fd0754442de3c87680ddbc6
disappeared <shenrz@jhintl.com>
2012-03-10 18:55:11 +0800

    新增问答模块。包括1、问题总览。2、问题详细页.3、首页显示模块。4、新建问题模块。5、回答问题模块。接下去逐一制作每个模块内容

---
+++ Quset/Lib/Action/QuestionAction.class.php
@@ -0,0 +1,30 @@
+<?php
+class QuestionAction extends CommonAction
+{   
+    //问题总览页面
+    function index()
+    {
+        $this->display();
+    }
+    //问题详细页
+    function view()
+    {
+        $this->display();
+    }
+    //首页显示模块
+    function ShowIndex()
+    {
+        $this->display();
+    }
+    //增加问题
+    function add()
+    {
+        $this->display();
+    }
+    //回答问题
+    function answer()
+    {
+        $this->display();
+    }
+}
+?>
