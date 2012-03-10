649f95e56ba75c50e7d73a226e25e5e7991920d7
disappeared <shenrz@jhintl.com>
2012-03-10 18:53:11 +0800

    新增common文件。以保证所有文件都是UTF-8格式

---
+++ Quset/Lib/Action/CommonAction.class.php
@@ -0,0 +1,14 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+class CommonAction extends Action
+{
+    public function _initialize()
+    {
+        header("Content-Type:text/html; charset=utf-8");
+    }
+}
+?>
